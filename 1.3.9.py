1d5b4b157b8f43e32c9ee33a0d486d926dc941e7
EthanOliver2nd <45764858+EthanOliver2nd@users.noreply.github.com>
2018-12-11 09:46:48 -0600

    Add files via upload

--- 1.3.9.py
+++ 1.3.9.py
@@ -102,24 +102,34 @@ def library():
             print("The Great War of The Internet Age” stands boldly at the top of the book. It was the date, December 2nd, 2018. T-Series the infamous Indian record company was remarkably close to passing Pewdiepie and becoming theNumber one most subscribed account on the YouTube platform. The 9 year olds were waging the good fight but it wasn’t enough, the gap was closing, and it was an only a matter of hours before the war was lost. It seemed all hope was lost, Mr. Beast had failed, Pewds best friends Jack and Mark had abandoned him. The 9-year-olds regrouped and revitalized their attack strategy. They came out the door swinging gaining a couple thousand subscribers but it wasn’t enough, T-Series was catching up and fast. Greyness loomed over the heads of the 9-year-olds, it seemed that finally that their king would be dethroned, but a miracle happened! Markiplier, Boogie2988, and Jacksepticeye had joined the fight, swinging the tide of battle, the war was not lost, reinforcements had come through this was a major turning point. Markiplier had posted a livestream of him continually talking and not shutting up ever until people subscribed to pewdiepie, and from there the 9-year-olds were off the the races. Jack tweeted a simple but direct poem encouraging his fans, and Davie504 played the Felix’s most hottest song, the T-Series disstrack for 10 hours straight, all collaborations of efforts were pulling through and pewdiepie had regained a comfortable subscriber lead. He ended up pulling 544k subscribers on december 2nd, a very important day in the war of Pewdiepie v. T-Series. Pewds had won this great battle but the war was still not over. Subscribe to Pewdiepie to make a difference again.")
         
         elif userAction.lower() == 'look around':
-            print('Only you stand in the library, faced with a daunting task')
+            print('Only you stand in the library, faced with a daunting task, at the corner of your eye you see a polaroid')
             
         elif userAction.lower() == 'go to desk':
-            print('There is a note, "According to the legend, the Devitus that roams this abandoned base, can be summoned')
-            print('You turn the note around, there is a blurred list')
+            print('There is a note, "According to the legend, the Devitus that roams this abandoned base, and can be summoned')
+            print('You turn the note around, there is a blurred list\n\n\n\n\n\n')
             
             
             
             
             
-            print('Step 1: O̴͕͇͕̗̐̀͆n̷̡̢̢̛͉̭̰̭̣͔͔͓̖̳͔̱̺͕̯̮̣̣͙̈́̃̓̇͆̆͌̉̍̉̆̊̋́̎̐̇͂̾͆͘̚ē̷̢̢̛͉̫͎͎͙̫̞̑͗͆͋͗̈́̈́̎̇͆̾̈́͑̂̚̚͠͠͝͠ ̸̛̛̛̤̯̗͓̲̾͒̐͌̄̓͊̂̏̈̂̑͝͠͝͝m̶̛̛̖̰͍̫̯̺͚̤̣̟̫̯̱͎̹̤̝͚͕͙̆͆́̿̒̓̉̍͂̎̐̓͝ͅͅu̵̯̝̠̯̣͎̞̥̘̙͔̥̇͑͆̈̋͐́͒͐͗̿͌̅͆̾̋̂̕͝͝͝s̵̛̰̺͐͌̂̄͐̄͐t̶̹̯̦̮̟̳̮̦̯̞͌́̏̔̓̐̅̈͋̚̚͘͠͝ ̶̡̛̯̞̝̬͍̭̏͌̔̈́̈͆̈́̆̀͊͝͝ͅỉ̵̢̖͍̻̩̱̗̥̘̫̬͔̘̱̘̼ͅn̶̨̢̛̠̮̘̦̦̯̝͎͇̥̞̺͎͚͕͙̏̒̓͗͛͗͐̾́̿̇͒̔̚̚͝f̷̛̯͕̗̩͎͈͓̲̭̥͇͇̝̘̟̬̜̰̬̟̯̗̻̂̃͒̂̏̿̅̊̒̃̔́̀̆̀̎͗̊̈́̀̇́̕ͅͅi̸̢̛̖̫͚̼̞̽̓͂̈́̆̓̇̔̈́̓͋͌́̐̚͘̚̕͜͝͝͝l̴̛̩͓̫̥̲̠̗̝͙͍̪͍̳̗̪̤͉̭̭̮̻̇̃͐̈́̆͆͐̃͆͆̈́͑̔̀̒̂͊̄̾̆̿̕͜͝ͅţ̴̡̧̛͔̟̯̣̪̟̠͔̩̙̹̝̣͚͕̓́̍r̶̗̭̒̔͐̂̅̈́͊̈́̎́̔̋̉̐̑̈́͒͌͜a̷̡̬̲͕̪̻̤̳̤̙̬̘͚͇̳̙̣̳̦̾̀̆̓̏̃̀͛̀̓̓́͋̉́̓̕͘̕͜͝͝t̷̢̛̥̳̽͑͂̑͋̔̓̆̓̈́̊̏͒e̸̢̫̘͓͙̼̭̪͔͙͍̝̥͖͓͎͖͇̙̻͆͛̔̉̒͆̆̽̌̄̒̚͘͜͜͜͠͝ ̷̨̨̮̼̮̼̥͉̼̫̳̻̺̫͚̖̣̯̘̠̠̖͈̽̃̐͛͆̀̏̓̾̈́̀́͠͝ͅț̶̡̧̛̠͎̥̉͊̿̃̔̔͗̀̀́̈́͂̅̂̕͠h̷̝͒̅ȩ̷̛̟͔͍̳̝̗̪͍̱͕̖͉̺͎̗͈̇̂̀͆̌̈́̀͋̔̔̅̿̌̕͝ ̸̼̹̹̻͉̱̭͈̥̟͚̼͕̹͙̱͔̣̓͋̊͊̉͊̉̏̇̇͐̑̈̀̔̋̌̂̂̅́͜͜͜͝ṟ̷̨̛͈̳̣͕̣̱͙̬̬͙͔͓̲̯̖̤͍͆̄̉̒͗o̵͔͕͙̘̙̾ǒ̴̪̝͐͆͆̓͆̓́̃́̄̐͑͘̚͝m̵͚̯͍͎̠̞̲͍̻̥̱̗̘̞̮̠͈̖̣͓̫̹͎͇͂̍̅̑̍̎̐̅̈́̚ ̶̢̲̩͈͖̪̬̮̦̞̬͉͔͚͂̌̈́̎͋̉̒̈́͜͝ẅ̸̧̗̞̘̙̤̰̠͍̼͎̥́̊̏͆̈́͑͗̄̓̑̚̚͜͝͝͝ḫ̶̨̢̪̻̜̩̝̞͖̣̬͎̜̾̉̒e̸̪̙͉̼͉̙̘͕̳̻̘̯̯͛̋̄́̈́͆̍̂̓̈̕̚͝ŗ̷̢̢̛͎̥͕̙̜̯̝̟̙̝̰̻̭̤͉͔̪̖͎̣̮̼̜̿̍̈́̔͐̓̉̅́͛̎̏͠ȇ̴̱͉͕̰̣̪͔̬̦͈̗̦͙̔͛͜ͅ ̶͚̣̦͔̣͇͔͕̣͉͙͙̠̯͇̀̈́̐̃͋͆̉̑́̈̄͘̚c̸͍͉̤̝̤̹̞͔͇̣̬̳͔̪͙͓̑ͅͅḩ̸͉̝̘̹̞̳̟̖̤͈̙̳͙͕͙̹̙͇̭̗̬̿̈́͑͛͂̔̋͊͌͘͝a̶̧͚͖̦̦̯̫̭̳͉̺̥̥͙̱͙̹͚͓͓̮̲̾̈́̃̑̓͌̍̈́̐̉̊͆̊̂̇͑͠͝ͅņ̷̧̛͍̝̬̞̲̟̮̦̮̫͚͕̜̘͓̏͗̌̓͒̃͌͐̕̕͝ͅģ̷̛͚̩͇̙̥̼̼͓̼̙͕͔͔̬̗̬̦͔͖̯͕͇͑͑̀̾̈́̇̽͗̚͜ė̷̢̛͚̖͖̹͕͖̙̅̌͌̔̏͂̓̐̓̍̄̊̌̓̐̔̕ͅs̴͖̀̏̊̊́͛̈́̄̈̎̂̋̈́͛̊̈̌̏̈́̎͝͝ ̴͉̞̺̖̲͉̬̻̟̤̲̻̱̮͍͚͕̗̖̰̥͋͆͒ͅơ̶̹͓͍͇̭̹̩̲̝̟̗̺̮͎̖̻̟̅̂̉̑͂̂̀͌̄̽̿͌̚̕͝ç̷̢̢̨͓͓̭͙̠̙̪͗̾̽̀͗̌͆͂͝c̴̢̰̳̫͎̬͎̤̲̩̟̻͔̩̳̰̀̄̄͗̎͑͂̀̀̈́́̚͜ư̸̢͉͙̤̖̩͇̼̫̍̓̈́̓͛̃̓̋͋̚̕̕͝͠ͅr̸͍̲͑̂̏͌̄.̷̨̼̠̖̟͔͕̞̩̗̪̲̯̠͖̅̂̽̾̂̏͗́̉̽̑͗͗̎̐̈́̔̇̒̃̃̀̎͊ͅ')
-           
+            print('Step 1: O̴͕͇͕̗̐̀͆n̷̡̢̢̛͉̭̰̭̣͔͔͓̖̳͔̱̺͕̯̮̣̣͙̈́̃̓̇͆̆͌̉̍̉̆̊̋́̎̐̇͂̾͆͘̚ē̷̢̢̛͉̫͎͎͙̫̞̑͗͆͋͗̈́̈́̎̇͆̾̈́͑̂̚̚͠͠͝͠ ̸̛̛̛̤̯̗͓̲̾͒̐͌̄̓͊̂̏̈̂̑͝͠͝͝m̶̛̛̖̰͍̫̯̺͚̤̣̟̫̯̱͎̹̤̝͚͕͙̆͆́̿̒̓̉̍͂̎̐̓͝ͅͅu̵̯̝̠̯̣͎̞̥̘̙͔̥̇͑͆̈̋͐́͒͐͗̿͌̅͆̾̋̂̕͝͝͝s̵̛̰̺͐͌̂̄͐̄͐t̶̹̯̦̮̟̳̮̦̯̞͌́̏̔̓̐̅̈͋̚̚͘͠͝ ̶̡̛̯̞̝̬͍̭̏͌̔̈́̈͆̈́̆̀͊͝͝ͅỉ̵̢̖͍̻̩̱̗̥̘̫̬͔̘̱̘̼ͅn̶̨̢̛̠̮̘̦̦̯̝͎͇̥̞̺͎͚͕͙̏̒̓͗͛͗͐̾́̿̇͒̔̚̚͝f̷̛̯͕̗̩͎͈͓̲̭̥͇͇̝̘̟̬̜̰̬̟̯̗̻̂̃͒̂̏̿̅̊̒̃̔́̀̆̀̎͗̊̈́̀̇́̕ͅͅi̸̢̛̖̫͚̼̞̽̓͂̈́̆̓̇̔̈́̓͋͌́̐̚͘̚̕͜͝͝͝l̴̛̩͓̫̥̲̠̗̝͙͍̪͍̳̗̪̤͉̭̭̮̻̇̃͐̈́̆͆͐̃͆͆̈́͑̔̀̒̂͊̄̾̆̿̕͜͝ͅţ̴̡̧̛͔̟̯̣̪̟̠͔̩̙̹̝̣͚͕̓́̍r̶̗̭̒̔͐̂̅̈́͊̈́̎́̔̋̉̐̑̈́͒͌͜a̷̡̬̲͕̪̻̤̳̤̙̬̘͚͇̳̙̣̳̦̾̀̆̓̏̃̀͛̀̓̓́͋̉́̓̕͘̕͜͝͝t̷̢̛̥̳̽͑͂̑͋̔̓̆̓̈́̊̏͒e̸̢̫̘͓͙̼̭̪͔͙͍̝̥͖͓͎͖͇̙̻͆͛̔̉̒͆̆̽̌̄̒̚͘͜͜͜͠͝ ̷̨̨̮̼̮̼̥͉̼̫̳̻̺̫͚̖̣̯̘̠̠̖͈̽̃̐͛͆̀̏̓̾̈́̀́͠͝ͅț̶̡̧̛̠͎̥̉͊̿̃̔̔͗̀̀́̈́͂̅̂̕͠h̷̝͒̅ȩ̷̛̟͔͍̳̝̗̪͍̱͕̖͉̺͎̗͈̇̂̀͆̌̈́̀͋̔̔̅̿̌̕͝ ̸̼̹̹̻͉̱̭͈̥̟͚̼͕̹͙̱͔̣̓͋̊͊̉͊̉̏̇̇͐̑̈̀̔̋̌̂̂̅́͜͜͜͝ṟ̷̨̛͈̳̣͕̣̱͙̬̬͙͔͓̲̯̖̤͍͆̄̉̒͗o̵͔͕͙̘̙̾ǒ̴̪̝͐͆͆̓͆̓́̃́̄̐͑͘̚͝m̵͚̯͍͎̠̞̲͍̻̥̱̗̘̞̮̠͈̖̣͓̫̹͎͇͂̍̅̑̍̎̐̅̈́̚ ̶̢̲̩͈͖̪̬̮̦̞̬͉͔͚͂̌̈́̎͋̉̒̈́͜͝ẅ̸̧̗̞̘̙̤̰̠͍̼͎̥́̊̏͆̈́͑͗̄̓̑̚̚͜͝͝͝ḫ̶̨̢̪̻̜̩̝̞͖̣̬͎̜̾̉̒e̸̪̙͉̼͉̙̘͕̳̻̘̯̯͛̋̄́̈́͆̍̂̓̈̕̚͝ŗ̷̢̢̛͎̥͕̙̜̯̝̟̙̝̰̻̭̤͉͔̪̖͎̣̮̼̜̿̍̈́̔͐̓̉̅́͛̎̏͠ȇ̴̱͉͕̰̣̪͔̬̦͈̗̦͙̔͛͜ͅ ̶͚̣̦͔̣͇͔͕̣͉͙͙̠̯͇̀̈́̐̃͋͆̉̑́̈̄͘̚c̸͍͉̤̝̤̹̞͔͇̣̬̳͔̪͙͓̑ͅͅḩ̸͉̝̘̹̞̳̟̖̤͈̙̳͙͕͙̹̙͇̭̗̬̿̈́͑͛͂̔̋͊͌͘͝a̶̧͚͖̦̦̯̫̭̳͉̺̥̥͙̱͙̹͚͓͓̮̲̾̈́̃̑̓͌̍̈́̐̉̊͆̊̂̇͑͠͝ͅņ̷̧̛͍̝̬̞̲̟̮̦̮̫͚͕̜̘͓̏͗̌̓͒̃͌͐̕̕͝ͅģ̷̛͚̩͇̙̥̼̼͓̼̙͕͔͔̬̗̬̦͔͖̯͕͇͑͑̀̾̈́̇̽͗̚͜ė̷̢̛͚̖͖̹͕͖̙̅̌͌̔̏͂̓̐̓̍̄̊̌̓̐̔̕ͅs̴͖̀̏̊̊́͛̈́̄̈̎̂̋̈́͛̊̈̌̏̈́̎͝͝ ̴͉̞̺̖̲͉̬̻̟̤̲̻̱̮͍͚͕̗̖̰̥͋͆͒ͅơ̶̹͓͍͇̭̹̩̲̝̟̗̺̮͎̖̻̟̅̂̉̑͂̂̀͌̄̽̿͌̚̕͝ç̷̢̢̨͓͓̭͙̠̙̪͗̾̽̀͗̌͆͂͝c̴̢̰̳̫͎̬͎̤̲̩̟̻͔̩̳̰̀̄̄͗̎͑͂̀̀̈́́̚͜ư̸̢͉͙̤̖̩͇̼̫̍̓̈́̓͛̃̓̋͋̚̕̕͝͠ͅr̸͍̲͑̂̏͌̄.̷̅̂̽̾ \n \n \n \n \n') 
             
             
             
             
             
             print('The list is too worn to read')
+            
+        elif userAction.lower() == 'look at polaroid':
+            print('You see him, the image has appeared, the Detivus at full might stares blankly into your eyes. Hope escapes you in this moment as the picture of the Detivus overwhelmes all thought.')
+            
+        elif userAction.lower() == 'go west':
+            exitRoom = True
+            cafe()
+        
+        else:
+            userAction = raw_input('Can\'t do that.')
+            
             
         
             
